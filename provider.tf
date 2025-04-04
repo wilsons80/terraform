4b422dceee6ee93877575ed309ae08a1e2c782bb
Wilson Souza <wilsons80@gmail.com>
2025-04-04 12:37:15 -0300

    Ajustando o provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.94.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "comunidadedevops-s3-terraform"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
