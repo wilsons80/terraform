746a9cd17c21c58c404c3845bac0171df345d6ce
Wilson Souza <wilsons80@gmail.com>
2025-04-05 11:46:11 -0300

    Comentando o funcionamento de passagem de variaveis nos modulos

--- modules.tf
+++ modules.tf
@@ -1,6 +1,10 @@
 module "eks_network" {
+  //Informa o path do modulo  
+  source = "./modules/network"
+
+  // declara as variáveis que serão passadas para dentro do modulo
+  // Serão todas as variáveis declaradas em //modules/network/variables.tf  
   project_name = var.project_name
   cidr_block   = var.cidr_block
-  source       = "./modules/network"
   tags         = local.tags
 }
