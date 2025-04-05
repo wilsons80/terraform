96357fa21d51356352714bb7506ec9c4be929e5a
Wilson Souza <wilsons80@gmail.com>
2025-04-05 11:35:52 -0300

    Configurando os Outputs para serem reaproveitados em outros modulos

---
+++ modules/network/output.tf
@@ -0,0 +1,16 @@
+output "subnet_pub_1a" {
+  value = aws_subnet.comunidadedevops-subnet-pubilc-1a.id
+}
+
+output "subnet_pub_1b" {
+  value = aws_subnet.comunidadedevops-subnet-pubilc-1b.id
+}
+
+
+output "subnet_priv_1a" {
+  value = aws_subnet.comunidadedevops-subnet-private-1a.id
+}
+
+output "subnet_priv_1b" {
+  value = aws_subnet.comunidadedevops-subnet-private-1b.id
+}
---
+++ output.tf
@@ -0,0 +1,16 @@
+output "subnet_pub_1a" {
+  value = module.eks_network.subnet_pub_1a
+}
+
+output "subnet_pub_1b" {
+  value = module.eks_network.subnet_pub_1b
+}
+
+
+output "subnet_private_1a" {
+  value = module.eks_network.subnet_priv_1a
+}
+
+output "subnet_private_1b" {
+  value = module.eks_network.subnet_priv_1b
+}
