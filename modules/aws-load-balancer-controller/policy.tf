f1369bc85a6f50fa2380cdc575cf8046de798e11
Wilson Souza <wilsons80@gmail.com>
2025-04-06 13:54:51 -0300

    Ajustando as TAGs

--- modules/aws-load-balancer-controller/policy.tf
+++ modules/aws-load-balancer-controller/policy.tf
@@ -1,5 +1,10 @@
 resource "aws_iam_policy" "eks_controller_policy" {
   name   = "${var.project_name}-aws-load-balancer-controller"
   policy = file("${path.module}/iam_policy.json")
-  tags   = var.tags
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-policy-alb-controller"
+    }
+  )
 }
