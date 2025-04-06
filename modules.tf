14cbf0b180e1e279067abc02d1046ecdc35b5d45
Wilson Souza <wilsons80@gmail.com>
2025-04-06 12:42:04 -0300

    Criando o modulo communs

--- modules.tf
+++ modules.tf
@@ -1,3 +1,7 @@
+module "eks_communs" {
+  source = "./modules/communs"
+}
+
 module "eks_network" {
   //Informa o path do modulo  
   source = "./modules/network"
@@ -6,7 +10,7 @@ module "eks_network" {
   // Serão todas as variáveis declaradas em //modules/network/variables.tf  
   project_name = var.project_name
   cidr_block   = var.cidr_block
-  tags         = local.tags
+  tags         = module.eks_communs.tags
 }
 
 
@@ -14,7 +18,7 @@ module "eks_cluster" {
   source = "./modules/cluster"
 
   project_name     = var.project_name
-  tags             = local.tags
+  tags             = module.eks_communs.tags
   public_subnet_1a = module.eks_network.subnet_pub_1a
   public_subnet_1b = module.eks_network.subnet_pub_1b
 
@@ -24,8 +28,9 @@ module "eks_cluster" {
 module "eks_managed_node_group" {
   source            = "./modules/managed-node-group"
   project_name      = var.project_name
-  tags              = local.tags
+  tags              = module.eks_communs.tags
   subnet_private_1a = module.eks_network.subnet_priv_1a
   subnet_private_1b = module.eks_network.subnet_priv_1b
   cluster_name      = module.eks_cluster.cluster_name
 }
+
